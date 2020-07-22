6c4e32d42c40a18a25d5323d9fc69349c397d551
tong shen <ts325@duke.edu>
2020-07-22 00:59:06 -0400

    return JSON format data as response

--- jupiter/src/main/java/rpc/SearchItem.java
+++ jupiter/src/main/java/rpc/SearchItem.java
@@ -1,10 +1,14 @@
 package rpc;
 
 import java.io.IOException;
+// import PrintWriter
+import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+// import JSONObject
+import org.json.JSONObject;
 
 /**
  * Servlet implementation class SearchItem
@@ -24,8 +28,16 @@ public class SearchItem extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		// return JSON format data as response
+		// tell the front-end the content type
+		response.setContentType("application/json");
+		// create an JSONObject and add key-value pairs
+		JSONObject obj = new JSONObject();
+		obj.put("username", "abcd");
+		obj.put("age", "26");
+		// return the JSONObject as the result to the front-end
+		PrintWriter writer = response.getWriter();
+		writer.print(obj);
 	}
 
 	/**
