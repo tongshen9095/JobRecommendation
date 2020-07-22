e11bb5b8844cd972528c4e98e1bf0657f13e2579
tong shen <ts325@duke.edu>
2020-07-22 02:35:46 -0400

    return two user info with JSONArray

--- jupiter/src/main/java/rpc/RecommendItem.java
+++ jupiter/src/main/java/rpc/RecommendItem.java
@@ -1,37 +1,60 @@
 package rpc;
 
+import java.io.PrintWriter;
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 /**
  * Servlet implementation class RecommendItem
  */
 public class RecommendItem extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public RecommendItem() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	public RecommendItem() {
+		super();
+		// TODO Auto-generated constructor stub
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("application/json");
+
+		JSONObject obj1 = new JSONObject();
+		obj1.put("name", "abcd");
+		obj1.put("address", "san francisco");
+		obj1.put("time", "01/01/2017");
+
+		JSONObject obj2 = new JSONObject();
+		obj2.put("name", "1234");
+		obj2.put("address", "san jose");
+		obj2.put("time", "01/02/2017");
+
+		JSONArray array = new JSONArray();
+		array.put(obj1);
+		array.put(obj2);
+
+		PrintWriter writer = response.getWriter();
+		writer.print(array);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
