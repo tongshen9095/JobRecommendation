33bebfbfc27d0371dd646397dbbe7a12b186ba94
tong shen <ts325@duke.edu>
2020-07-23 13:11:12 -0400

    import libraries, add two final

--- jupiter/src/main/java/external/GitHubClient.java
+++ jupiter/src/main/java/external/GitHubClient.java
@@ -1,5 +1,20 @@
 package external;
 
-public class GitHubClient {
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+
+public class GitHubClient {
+	private static final String URL_TEMPLATE = "https://jobs.github.com/positions.json?description=%s&lat=%s&long=%s";
+	private static final String DEFAULT_KEYWORD = "developer";
 }
