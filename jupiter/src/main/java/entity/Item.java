06a9a87dbe96c8af986b4c9cdf85f0d4225a1d48
tong shen <ts325@duke.edu>
2020-07-24 00:30:49 -0400

    write the constructor with BuildPattern

--- jupiter/src/main/java/entity/Item.java
+++ jupiter/src/main/java/entity/Item.java
@@ -10,6 +10,15 @@ public class Item {
 	private String imageUrl;
 	private String url;
 	
+	private Item(ItemBuilder builder) {
+		this.itemId = builder.itemId;
+		this.name = builder.name;
+		this.address = builder.address;
+		this.keywords = builder.keywords;
+		this.imageUrl = builder.imageUrl;
+		this.url = builder.url;
+	}
+	
 	public String getItemId() {
 		return itemId;
 	}
@@ -29,5 +38,34 @@ public class Item {
 		return url;
 	}
 	
-
+	public static class ItemBuilder {
+		private String itemId;
+		private String name;
+		private String address;
+		private Set<String> keywords;
+		private String imageUrl;
+		private String url;
+		
+		public void setItemId(String itemId) {
+			this.itemId = itemId;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public void setAddress(String address) {
+			this.address = address;
+		}
+		public void setKeywords(Set<String> keywords) {
+			this.keywords = keywords;
+		}
+		public void setImageUrl(String imageUrl) {
+			this.imageUrl = imageUrl;
+		}
+		public void setUrl(String url) {
+			this.url = url;
+		}
+		public Item build() {
+			return new Item(this);
+		}
+	}
 }
