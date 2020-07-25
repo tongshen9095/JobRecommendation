0ce7c4ac9e8c687824d94634ce379c4ce85ffc13
tong shen <ts325@duke.edu>
2020-07-25 19:03:31 -0400

    add item to jsonobject method

--- jupiter/src/main/java/entity/Item.java
+++ jupiter/src/main/java/entity/Item.java
@@ -1,6 +1,10 @@
 package entity;
 
 import java.util.Set;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import lombok.Builder;
 import lombok.Getter;
 
@@ -13,4 +17,15 @@ public class Item {
 	private Set<String> keywords;
 	private String imageUrl;
 	private String url;
+	
+	public JSONObject toJSONObject() {
+		JSONObject obj = new JSONObject();
+		obj.put("item_id", itemId);
+		obj.put("name", name);
+		obj.put("address", address);
+		obj.put("keywords", new JSONArray(keywords));
+		obj.put("image_url", imageUrl);
+		obj.put("url", url);
+		return obj;
+	}
 }
