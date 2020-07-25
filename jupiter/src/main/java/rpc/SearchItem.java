116c8743986a782cad83bc0e9b4b07e769f41ca4
tong shen <ts325@duke.edu>
2020-07-25 19:06:09 -0400

    receive item list in doGet

--- jupiter/src/main/java/rpc/SearchItem.java
+++ jupiter/src/main/java/rpc/SearchItem.java
@@ -1,11 +1,15 @@
 package rpc;
 
 import java.io.IOException;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.json.JSONArray;
+
+import entity.Item;
 import external.GitHubClient;
 
 /**
@@ -13,7 +17,7 @@ import external.GitHubClient;
  */
 public class SearchItem extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
+	
 	/**
 	 * @see HttpServlet#HttpServlet()
 	 */
@@ -31,7 +35,11 @@ public class SearchItem extends HttpServlet {
 		double lat = Double.parseDouble(request.getParameter("lat"));
 		double lon = Double.parseDouble(request.getParameter("lon"));
 		GitHubClient client = new GitHubClient();
-		JSONArray array = client.search(lat, lon, null);
+		List<Item> items = client.search(lat, lon, null);
+		JSONArray array = new JSONArray();
+		for (Item item : items) {
+			array.put(item.toJSONObject());
+		}
 		RpcHelper.writeJsonArray(response, array);
 	}
 
