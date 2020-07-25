a343362d627ba5a8cba90dfec6058d350f48b2b6
tong shen <ts325@duke.edu>
2020-07-24 20:14:17 -0400

    use lombok to simlify item constructor

--- jupiter/src/main/java/entity/Item.java
+++ jupiter/src/main/java/entity/Item.java
@@ -1,7 +1,11 @@
 package entity;
 
 import java.util.Set;
+import lombok.Builder;
+import lombok.Getter;
 
+@Getter
+@Builder
 public class Item {
 	private String itemId;
 	private String name;
@@ -9,63 +13,4 @@ public class Item {
 	private Set<String> keywords;
 	private String imageUrl;
 	private String url;
-	
-	private Item(ItemBuilder builder) {
-		this.itemId = builder.itemId;
-		this.name = builder.name;
-		this.address = builder.address;
-		this.keywords = builder.keywords;
-		this.imageUrl = builder.imageUrl;
-		this.url = builder.url;
-	}
-	
-	public String getItemId() {
-		return itemId;
-	}
-	public String getName() {
-		return name;
-	}
-	public String getAddress() {
-		return address;
-	}
-	public Set<String> getKeywords() {
-		return keywords;
-	}
-	public String getImageUrl() {
-		return imageUrl;
-	}
-	public String getUrl() {
-		return url;
-	}
-	
-	public static class ItemBuilder {
-		private String itemId;
-		private String name;
-		private String address;
-		private Set<String> keywords;
-		private String imageUrl;
-		private String url;
-		
-		public void setItemId(String itemId) {
-			this.itemId = itemId;
-		}
-		public void setName(String name) {
-			this.name = name;
-		}
-		public void setAddress(String address) {
-			this.address = address;
-		}
-		public void setKeywords(Set<String> keywords) {
-			this.keywords = keywords;
-		}
-		public void setImageUrl(String imageUrl) {
-			this.imageUrl = imageUrl;
-		}
-		public void setUrl(String url) {
-			this.url = url;
-		}
-		public Item build() {
-			return new Item(this);
-		}
-	}
 }
