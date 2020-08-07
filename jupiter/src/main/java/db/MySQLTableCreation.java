dc2cdd952464a8adea9bd9c57451fae3d6dbc119
tong shen <ts325@duke.edu>
2020-08-07 18:27:46 -0400

    create four tables

--- jupiter/src/main/java/db/MySQLTableCreation.java
+++ jupiter/src/main/java/db/MySQLTableCreation.java
@@ -24,6 +24,43 @@ public class MySQLTableCreation {
             stmt.executeUpdate(sql);
             sql = "DROP TABLE IF EXISTS users";
             stmt.executeUpdate(sql);
+            // step3: create new tables
+            sql = "CREATE TABLE items ("
+            		+ "item_id VARCHAR(255) NOT NULL,"
+            		+ "name VARCHAR(255),"
+            		+ "address VARCHAR(255),"
+            		+ "image_url VARCHAR(255),"
+            		+ "url VARCHAR(255),"
+            		+ "PRIMARY KEY (item_id)"
+            		+ ")";
+            stmt.executeLargeUpdate(sql);
+            
+			sql = "CREATE TABLE users ("
+					+ "user_id VARCHAR(255) NOT NULL,"
+					+ "password VARCHAR(255) NOT NULL,"
+					+ "first_name VARCHAR(255),"
+					+ "last_name VARCHAR(255),"
+					+ "PRIMARY KEY (user_id)"
+					+ ")";
+			stmt.executeUpdate(sql);
+			
+			sql = "CREATE TABLE keywords ("
+					+ "item_id VARCHAR(255) NOT NULL,"
+					+ "keyword VARCHAR(255) NOT NULL,"
+					+ "PRIMARY KEY (item_id, keyword),"
+					+ "FOREIGN KEY (item_id) REFERENCES items(item_id)"
+					+ ")";
+			stmt.executeUpdate(sql);
+            
+			sql = "CREATE TABLE history ("
+					+ "user_id VARCHAR(255) NOT NULL,"
+					+ "item_id VARCHAR(255) NOT NULL,"
+					+ "last favor_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,"
+					+ "FOREIGN KEY (user_id, item_id),"
+					+ "FOREIGN KEY (user_id) REFERENCES users(user_id),"
+					+ "FOREIGN KEY (item_id) REFERENCES items(item_id)"
+					+ ")";
+			stmt.executeUpdate(sql);
             
             //
             conn.close();
