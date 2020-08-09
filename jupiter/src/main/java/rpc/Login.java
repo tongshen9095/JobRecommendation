bf2c69a49da1e05ae19aa6e4b92e51b2016e79ef
tong shen <ts325@duke.edu>
2020-08-09 18:11:45 -0400

    update dopost

--- jupiter/src/main/java/rpc/Login.java
+++ jupiter/src/main/java/rpc/Login.java
@@ -5,6 +5,12 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.commons.io.IOUtils;
+import org.json.JSONObject;
+
+import db.MySQLConnection;
 
 /**
  * Servlet implementation class Login
@@ -32,8 +38,26 @@ public class Login extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		JSONObject input = new JSONObject(IOUtils.toString(request.getReader()));
+		String userId = input.getString("uer_id");
+		String password = input.getString("password");
+		MySQLConnection conn = new MySQLConnection();
+		JSONObject obj = new JSONObject();
+		if (conn.verifyLogin(userId, password)) {
+			// create a new session
+			HttpSession session = request.getSession();
+			// store use_id into the session
+			session.setAttribute("user_id", userId);
+			obj.put("status", "ok")
+			.put("user_id", userId)
+			.put("name", conn.getFullname(userId));	
+		}
+		else {
+			obj.put("status", "Login failed, user id and password does not match.");
+			response.setStatus(401);
+		}
+		conn.close();
+		RpcHelper.writeJsonObject(response, obj);
 	}
 
 }
