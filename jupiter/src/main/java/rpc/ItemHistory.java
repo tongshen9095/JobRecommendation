492eeab404b65e01f14f065a4bda364c6ea99467
tong shen <ts325@duke.edu>
2020-08-08 22:13:22 -0400

    add doGet

--- jupiter/src/main/java/rpc/ItemHistory.java
+++ jupiter/src/main/java/rpc/ItemHistory.java
@@ -1,11 +1,14 @@
 package rpc;
 
 import java.io.IOException;
+import java.util.Set;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.apache.commons.io.IOUtils;
+import org.json.JSONArray;
 import org.json.JSONObject;
 import db.MySQLConnection;
 import entity.Item;
@@ -29,8 +32,18 @@ public class ItemHistory extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		String userId = request.getParameter("user_id");
+		MySQLConnection conn = new MySQLConnection();
+		Set<Item> items = conn.getFavItems(userId);
+		conn.close();
+		JSONArray array = new JSONArray();
+		for (Item item : items) {
+			JSONObject obj = item.toJSONObject();
+			// tell front-end to show a red heart indicating fav
+			obj.put("favorite", true);
+			array.put(obj);
+		}
+		RpcHelper.writeJsonArray(response, array);
 	}
 
 	/**
