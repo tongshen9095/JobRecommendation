9b5a838d8eaaec832bbeaf8db90098650f9bf0cf
tong shen <ts325@duke.edu>
2020-08-11 13:41:28 -0400

    only show login info and hide the rest

--- jupiter/src/main/webapp/scripts/main.js
+++ jupiter/src/main/webapp/scripts/main.js
@@ -1,8 +1,47 @@
-// step 3
 (function() {
+	// step 3
 	var user_id = "1111";
 	var user_fullname = "John";
 	var lng = -122.08;
 	var lat = 37.38;
-	console.log(user_id);
+	
+	// step4: only show login info and hide the rest
+	function init() {
+		validateSession();
+	}
+	function validateSession() {
+		onSessionInvalid();
+	}
+		function onSessionInvalid() {
+		var loginForm = document.querySelector('#login-form');
+		var registerForm = document.querySelector('#register-form');
+		var itemNav = document.querySelector('#item-nav');
+		var itemList = document.querySelector('#item-list');
+		var avatar = document.querySelector('#avatar');
+		var welcomeMsg = document.querySelector('#welcome-msg');
+		var logoutBtn = document.querySelector('#logout-link');
+
+		hideElement(itemNav);
+		hideElement(itemList);
+		hideElement(avatar);
+		hideElement(logoutBtn);
+		hideElement(welcomeMsg);
+		hideElement(registerForm);
+
+		clearLoginError();
+		showElement(loginForm);
+	}
+	function hideElement(element) {
+		element.style.display = 'none';
+	}
+	function clearLoginError() {
+		document.querySelector('#login-error').innerHTML = '';
+	}
+	function showElement(element, style) {
+		var displayStyle = style ? style : 'block';
+		element.style.display = displayStyle;
+	}
+	
+	init();
+	
 })();
