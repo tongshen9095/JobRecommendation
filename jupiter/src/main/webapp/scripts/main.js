0bb610f2110aaff4d62a6420b5c6a43beb75cd63
tong shen <ts325@duke.edu>
2020-08-13 16:17:29 -0400

    switch between register and login

--- jupiter/src/main/webapp/scripts/main.js
+++ jupiter/src/main/webapp/scripts/main.js
@@ -5,18 +5,19 @@
 	var lng = -122.08;
 	var lat = 37.38;
 	
-	// step4: only show login info and hide the rest
 	function init() {
-		// add event listener
 		document.querySelector('#login-form-btn').addEventListener('click',
 			onSessionInvalid);
 		document.querySelector('#register-form-btn').addEventListener('click',
 			showRegisterForm);
 		validateSession();
 	}
+	
 	function validateSession() {
 		onSessionInvalid();
 	}
+	
+	// only show login info
 	function onSessionInvalid() {
 		var loginForm = document.querySelector('#login-form');
 		var registerForm = document.querySelector('#register-form');
@@ -36,12 +37,40 @@
 		clearLoginError();
 		showElement(loginForm);
 	}
+	
+	// only show register form
+	function showRegisterForm() {
+		var loginForm = document.querySelector('#login-form');
+		var registerForm = document.querySelector('#register-form');
+		var itemNav = document.querySelector('#item-nav');
+		var itemList = document.querySelector('#item-list');
+		var avatar = document.querySelector('#avatar');
+		var welcomeMsg = document.querySelector('#welcome-msg');
+		var logoutBtn = document.querySelector('#logout-link');
+
+		hideElement(itemNav);
+		hideElement(itemList);
+		hideElement(avatar);
+		hideElement(logoutBtn);
+		hideElement(welcomeMsg);
+		hideElement(loginForm);
+
+		clearRegisterResult();
+		showElement(registerForm);
+	}
+
+	function clearRegisterResult() {
+		document.querySelector('#register-result').innerHTML = '';
+	}
+
 	function hideElement(element) {
 		element.style.display = 'none';
 	}
+	
 	function clearLoginError() {
 		document.querySelector('#login-error').innerHTML = '';
 	}
+	
 	function showElement(element, style) {
 		var displayStyle = style ? style : 'block';
 		element.style.display = displayStyle;
